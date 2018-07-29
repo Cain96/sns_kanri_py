a0806480da3a8edfbd04ccc6d5c59cc0c88a2883
Cain <Cain96@users.noreply.github.com>
2018-07-29 20:19:38 +0900

    fixed record output (#17)
    *  fixed output record

--- sns_kanri/sns/urls.py
+++ sns_kanri/sns/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from sns_kanri.sns import views
-from sns_kanri.sns.views import RecordViewSet, SNSViewSet
+from sns_kanri.sns.views import RecordOutputViewSet, RecordViewSet, SNSViewSet
 
 router = DefaultRouter()
 router.register('sns', SNSViewSet)
 router.register('record', RecordViewSet)
+router.register('output', RecordOutputViewSet)
 
 statistic_urlpatterns = [
     path('', views.StatisticView.as_view(), name='list')
